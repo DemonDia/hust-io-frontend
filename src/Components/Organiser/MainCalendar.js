2df233b020cdd7c76b1840b955c9cf367b3f96ee
Setsuna Yukiii <siang-meng@live.com>
2022-12-11 20:49:43 +0800

    calendar component working

--- src/Components/Organiser/MainCalendar.js
+++ src/Components/Organiser/MainCalendar.js
@@ -1,9 +1,193 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 function MainCalendar(props) {
+    const [loading, setLoading] = useState(true);
+    // year and month in numbers
+    const [year, setYear] = useState(null);
+    const [month, setMonth] = useState(null);
+    const [days, setDays] = useState(0);
+    const [calendarDays, setCalendarDays] = useState([]);
+    const monthArr = [
+        "Select A Month",
+        "January",
+        "Feburary",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const dayArr = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+    const loadCalendar = (year, month) => {
+        setLoading(true);
+        const currDate = new Date(year, month, 0);
+        var noOfDays = currDate.getDate();
+        setDays(noOfDays);
+        // days --> first of each month
+        var firstDayOfMonth = new Date(year, month-1, 1);
+        var weekRows = [];
+        var dayCounts = 0;
+        var weekCounts = 0;
+        while (dayCounts < noOfDays) {
+            var weekRow = [];
+            // day of the week
+            for (var day = 0; day < 7; day++) {
+                // first week
+                if (weekCounts == 0) {
+                    if (day < firstDayOfMonth.getDay()) {
+                        weekRow.push(" ");
+                    } else {
+                        dayCounts += 1;
+                        weekRow.push(dayCounts);
+                    }
+                } else {
+                    if (dayCounts >= noOfDays) {
+                        weekRow.push(" ");
+                    } else {
+                        dayCounts += 1;
+                        weekRow.push(dayCounts);
+                    }
+                }
+            }
+            weekRows.push(weekRow);
+            weekCounts += 1;
+        }
+        setCalendarDays(weekRows);
+        setLoading(false);
+    };
+
+    const changeYear = (change) => {
+        const newYear = year + change;
+        setYear(newYear);
+        loadCalendar(newYear, month);
+    };
+
+    const changeMonth = (newMonth) => {
+        if (newMonth != 0) {
+            setMonth(newMonth);
+            loadCalendar(year, newMonth);
+        }
+    };
+    useEffect(() => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth()+1;
+        setYear(currentYear);
+        setMonth(currentMonth);
+        loadCalendar(currentYear, currentMonth);
+    }, []);
     return (
         <div>
             <h1>Calendar</h1>
+            <h2 className="title">
+                {monthArr[month]} {year}
+            </h2>
+            Year:{year} Month:{month} Days:{days}
+            <div className="container calendarContainer">
+                <div className="columns">
+                    <div className="column is-half">
+                        <label>Year:</label>
+                        <div id="yearField">
+                            <div className="control yearChangeBtnContainer">
+                                <button
+                                    className="button yearChangeBtn"
+                                    onClick={() => {
+                                        changeYear(-1);
+                                    }}
+                                >
+                                    -
+                                </button>
+                            </div>
+                            <div className="control">
+                                <input
+                                    className="input is-focused"
+                                    type="text"
+                                    placeholder="Focused input"
+                                    value={year}
+                                    disabled
+                                    style={{ background: "white" }}
+                                />
+                            </div>
+                            <div className="control yearChangeBtnContainer">
+                                <button
+                                    className="button yearChangeBtn"
+                                    onClick={() => {
+                                        changeYear(1);
+                                    }}
+                                >
+                                    +
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="column is-half">
+                        <label>Month:</label>
+                        <br></br>
+                        <div className="select is-focused">
+                            <select
+                                value={month}
+                                onChange={(e) => {
+                                    changeMonth(e.target.value);
+                                }}
+                            >
+                                {monthArr.map((month, index) => {
+                                    return (
+                                        <option value={index} key={index}>
+                                            {month}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                {!loading ? <></> : <></>}
+
+                <table className="calendar">
+                    <thead>
+                        <tr>
+                            {dayArr.map((day, index) => {
+                                return (
+                                    <th key={index}>
+                                        <div className="calendarHeaderCell">
+                                            {day}
+                                        </div>
+                                    </th>
+                                );
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {calendarDays.map((calendarWeek, index) => {
+                            return (
+                                <tr className="row calendarRow" key={index}>
+                                    {calendarWeek.map((calendarDay, index) => {
+                                        return calendarDay == " " ? (
+                                            <td key={index}>
+                                                <div className="calendarCellDisabled"></div>
+                                            </td>
+                                        ) : (
+                                            <td key={index}>
+                                                <Link>
+                                                    <div className="calendarCell">
+                                                        {calendarDay}
+                                                    </div>
+                                                </Link>
+                                            </td>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
--- src/Pages/Organiser/OrganiserPage.js
+++ src/Pages/Organiser/OrganiserPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import MainCalendar from "../../Components/Organiser/MainCalendar"
 function OrganiserPage(props) {
     return (
         <div>
+            <h1>Organiser</h1>
+            <MainCalendar/>
             
         </div>
     );
