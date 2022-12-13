8298e5edbe06af09af8330fc5c0ce3da18069d62
Setsuna Yukiii <siang-meng@live.com>
2022-12-13 19:45:22 +0800

    fixed option boxes in homepage (refinement)

--- src/Components/Home/OptionBox.js
+++ src/Components/Home/OptionBox.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 function OptionBox(props) {
     return (
-        <div className="contaner is-two-quarter-desktop column">
+        <div className="contaner is-two-quarter-tablet column">
             <Link to={props.option.link}>
                 <div className="box optionBox">
                     <h1 className="subtitle">{props.option.name}</h1>
