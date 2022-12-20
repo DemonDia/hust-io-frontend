d994c767a3e125687139ba004a186b3fa712f2c5
Setsuna Yukiii <siang-meng@live.com>
2022-12-20 11:53:20 +0800

    quiz list

--- src/Components/Quiz/QuizList.js
+++ src/Components/Quiz/QuizList.js
@@ -1,10 +1,111 @@
-import React from 'react';
-
-function QuizList(props) {
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+function QuizList({ Contents, DeleteContent }) {
+    // ============search as you type============
+    const [search, setSearch] = useState("");
+    // ============filter in asc and desc============
+    const [filter, setFilter] = useState(0);
+    const filterOptions = ["A-Z", "Z-A"];
+    // ============delete event============
+    const deleteEvent = async (quizId) => {
+        await DeleteContent(quizId);
+    };
     return (
         <div>
-            <h1 className="title">Quiz List</h1>
-            
+            <div className="listComponent">
+                <div className="columns">
+                    <div className="column is-half-tablet">
+                        <label>Search:</label>
+                        <div>
+                            <div className="control">
+                                <input
+                                    className="input is-focused"
+                                    type="text"
+                                    placeholder="Seach by name..."
+                                    value={search}
+                                    onChange={(e) => {
+                                        setSearch(e.target.value);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="column is-one-quarter-tablet is-half-mobile">
+                        <label>Filter:</label>
+                        <br></br>
+                        <div className="select is-focused">
+                            <select
+                                value={filter}
+                                onChange={(e) => {
+                                    setFilter(e.target.value);
+                                }}
+                            >
+                                {filterOptions.map((filterOption, index) => {
+                                    return (
+                                        <option value={index} key={index}>
+                                            {filterOption}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    </div>
+                    <div
+                        className="column is-one-quarter-tablet is-half-mobile"
+                        style={{ alignSelf: "center" }}
+                    >
+                        <Link className="button button-link" to="/quizzes/add">
+                            Add
+                        </Link>
+                    </div>
+                </div>
+                {Contents.length > 0 ? (
+                    <>
+                        {Contents.filter((content) => {
+                            return content.quizName
+                                .toLowerCase()
+                                .includes(search.toLowerCase());
+                        })
+                            .sort((a, b) =>
+                                filter == 0
+                                    ? a.quizName.toLowerCase() >
+                                      b.quizName.toLowerCase()
+                                        ? 1
+                                        : -1
+                                    : b.quizName.toLowerCase() >
+                                      a.quizName.toLowerCase()
+                                    ? 1
+                                    : -1
+                            )
+                            .map((content) => {
+                                const {  quizName, _id } = content;
+                                return (
+                                    <div key={_id} className="listItem">
+                                        <div className="listItem-contents">
+                                            <h5 className="subtitle">
+                                                {quizName}
+                                            </h5>
+                                        </div>
+                                        <div className="listItem-buttons">
+                                            <Link to={`/quizzes/${_id}`}>
+                                                Edit
+                                            </Link>
+                                            <Link
+                                                onClick={() => deleteEvent(_id)}
+                                            >
+                                                Delete
+                                                {/* <DeleteIcon/> */}
+                                            </Link>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </>
+                ) : (
+                    <h2 className="title">Nothing here</h2>
+                )}
+            </div>
         </div>
     );
 }
--- src/Pages/Quiz/QuizListPage.js
+++ src/Pages/Quiz/QuizListPage.js
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { defaultAuthCheck } from "../../AuthCheck";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import QuizList from "../../Components/Quiz/QuizList";
+import { mainContext } from "../../Contexts/mainContext";
+
 function QuizListPage(props) {
+    const currentToken = localStorage.getItem("loginToken");
+    const { setUserId } = useContext(mainContext);
+    const [loading, setLoading] = useState(true);
+    const [currUserId, setCurrUserId] = useState(null);
+    const [quizzes, setQuizzes] = useState([]);
+    const navigate = useNavigate();
+    const loadUserQuizzes = async (userId) => {
+        const getQuizResults = await axios.get(
+            process.env.REACT_APP_BACKEND_API + `/quizzes/${userId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${currentToken}`,
+                },
+            }
+        );
+        if (getQuizResults.data.success) {
+            console.log(getQuizResults.data.data);
+            setQuizzes(getQuizResults.data.data);
+        }
+    };
+    const loadPage = async () => {
+        await defaultAuthCheck(navigate).then(async (result) => {
+            if (result.data.success) {
+                setUserId(result.data.id);
+                setCurrUserId(result.data.id);
+                await loadUserQuizzes(result.data.id);
+                setLoading(false);
+            }
+        });
+    };
+    const deleteQuiz = async (quizId) => {
+        await axios
+            .delete(process.env.REACT_APP_BACKEND_API + `/quizzes/${quizId}`, {
+                headers: { Authorization: `Bearer ${currentToken}` },
+                data: {
+                    userId: currUserId,
+                },
+            })
+            .then(async (res) => {
+                if (res.data.success) {
+                    alert("Successfully deleted");
+                    await loadUserQuizzes(currUserId);
+                } else {
+                    alert("Failed to delete");
+                }
+            })
+            .catch((err) => {
+                alert("Failed to delete");
+            });
+    };
+    useEffect(() => {
+        loadPage();
+    }, []);
     return (
         <div>
-            <h1 className="title">Quiz List Page</h1>
-            <QuizList/>
+            <h1 className="title">All Quizzes</h1>
+            {loading ? (
+                <>Loading ...</>
+            ) : (
+                <>
+                    <QuizList Contents={quizzes} DeleteContent={deleteQuiz} />
+                </>
+            )}
         </div>
+        // <div>
+        //     <h1 className="title">Quiz List Page</h1>
+        //     <QuizList />
+        // </div>
     );
 }
 
