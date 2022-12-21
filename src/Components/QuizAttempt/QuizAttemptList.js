17d32b9434ea5243fbec9def2b4f8fb1e75e6ace
Setsuna Yukiii <siang-meng@live.com>
2022-12-21 17:06:16 +0800

    refactoring of quiz management page

--- src/Components/QuizAttempt/QuizAttemptList.js
+++ src/Components/QuizAttempt/QuizAttemptList.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-function QuizList({ Contents, DeleteContent }) {
+function QuizAttemptList({ Contents, DeleteContent }) {
     // ============search as you type============
     const [search, setSearch] = useState("");
     // ============filter in asc and desc============
     const [filter, setFilter] = useState(0);
-    const filterOptions = ["A-Z", "Z-A"];
+    const filterOptions = ["Latest", "Earliest", "A-Z", "Z-A"];
     // ============delete event============
-    const deleteEvent = async (quizId) => {
+    const deleteAttempt = async (quizId) => {
         await DeleteContent(quizId);
     };
     return (
@@ -51,14 +51,6 @@ function QuizList({ Contents, DeleteContent }) {
                             </select>
                         </div>
                     </div>
-                    <div
-                        className="column is-one-quarter-tablet is-half-mobile"
-                        style={{ alignSelf: "center" }}
-                    >
-                        <Link className="button button-link" to="/quizzes/add">
-                            Add
-                        </Link>
-                    </div>
                 </div>
                 {Contents.length > 0 ? (
                     <>
@@ -69,6 +61,14 @@ function QuizList({ Contents, DeleteContent }) {
                         })
                             .sort((a, b) =>
                                 filter == 0
+                                    ? b.isoDate > a.isoDate
+                                        ? 1
+                                        : -1
+                                    : filter == 1
+                                    ? a.isoDate > b.isoDate
+                                        ? 1
+                                        : -1
+                                    : filter == 2
                                     ? a.quizName.toLowerCase() >
                                       b.quizName.toLowerCase()
                                         ? 1
@@ -79,23 +79,46 @@ function QuizList({ Contents, DeleteContent }) {
                                     : -1
                             )
                             .map((content) => {
-                                const { quizName, _id } = content;
+                                const {
+                                    quizName,
+                                    _id,
+                                    attemptStatus,
+                                    isoDate,
+                                } = content;
+                                const date = new Date(isoDate)
+                                const year = date.getFullYear()
+                                const month = date.getMonth()
+                                const day = date.getDay()
+                                const hour = date.getHours()
+                                const minute = date.getMinutes()
+                                const dateString = `${day}-${month}-${year}, ${hour}:${minute}`
                                 return (
                                     <div key={_id} className="listItem">
                                         <div className="listItem-contents">
                                             <h5 className="subtitle">
                                                 {quizName}
                                             </h5>
+                                            Attempted at: {dateString}
                                         </div>
                                         <div className="listItem-buttons">
-                                            <Link to={`/quizzes/attempt/add/${_id}`}>
-                                                Attempt
-                                            </Link>
-                                            <Link to={`/quizzes/${_id}`}>
-                                                Edit
+                                            <Link
+                                                to={`/quizzes/attempt/${_id}`}
+                                            >
+                                                {attemptStatus == 1 ? (
+                                                    <>Continue</>
+                                                ) : (
+                                                    <>
+                                                        {attemptStatus ==
+                                                        2 ? (
+                                                            <>Mark</>
+                                                        ) : (
+                                                            <>View </>
+                                                        )}
+                                                    </>
+                                                )}
                                             </Link>
                                             <Link
-                                                onClick={() => deleteEvent(_id)}
+                                                onClick={() => deleteAttempt(_id)}
                                             >
                                                 Delete
                                             </Link>
@@ -112,4 +135,4 @@ function QuizList({ Contents, DeleteContent }) {
     );
 }
 
-export default QuizList;
+export default QuizAttemptList;
--- src/Pages/Quiz/QuizListPage.js
+++ src/Pages/Quiz/QuizListPage.js
@@ -3,15 +3,38 @@ import { defaultAuthCheck } from "../../AuthCheck";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import QuizList from "../../Components/Quiz/QuizList";
+import QuizAttemptList from "../../Components/QuizAttempt/QuizAttemptList";
 import { mainContext } from "../../Contexts/mainContext";
 
 function QuizListPage(props) {
+    const navigate = useNavigate();
     const currentToken = localStorage.getItem("loginToken");
     const { setUserId } = useContext(mainContext);
     const [loading, setLoading] = useState(true);
     const [currUserId, setCurrUserId] = useState(null);
+    const loadPage = async () => {
+        await defaultAuthCheck(navigate).then(async (result) => {
+            if (result.data.success) {
+                setUserId(result.data.id);
+                setCurrUserId(result.data.id);
+                await loadUserQuizzes(result.data.id);
+                await loadUserQuizAttempts(result.data.id);
+                setLoading(false);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadPage();
+    }, []);
+
+    // ================tabs to toggle================
+    const [tab, setTab] = useState(0);
+    const tabs = ["Quizzes", "Quiz Attempts"];
+
+    // ================quizzes================
     const [quizzes, setQuizzes] = useState([]);
-    const navigate = useNavigate();
+
     const loadUserQuizzes = async (userId) => {
         const getQuizResults = await axios.get(
             process.env.REACT_APP_BACKEND_API + `/quizzes/${userId}`,
@@ -25,16 +48,7 @@ function QuizListPage(props) {
             setQuizzes(getQuizResults.data.data);
         }
     };
-    const loadPage = async () => {
-        await defaultAuthCheck(navigate).then(async (result) => {
-            if (result.data.success) {
-                setUserId(result.data.id);
-                setCurrUserId(result.data.id);
-                await loadUserQuizzes(result.data.id);
-                setLoading(false);
-            }
-        });
-    };
+
     const deleteQuiz = async (quizId) => {
         await axios
             .delete(process.env.REACT_APP_BACKEND_API + `/quizzes/${quizId}`, {
@@ -55,17 +69,88 @@ function QuizListPage(props) {
                 alert("Failed to delete");
             });
     };
-    useEffect(() => {
-        loadPage();
-    }, []);
+
+    // ================quiz attempts================
+    const [quizAttempts, setQuizAttempts] = useState([]);
+
+    const loadUserQuizAttempts = async (userId) => {
+        const getQuizResults = await axios.get(
+            process.env.REACT_APP_BACKEND_API + `/quizAttempts/${userId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${currentToken}`,
+                },
+            }
+        );
+        if (getQuizResults.data.success) {
+            setQuizAttempts(getQuizResults.data.data);
+        }
+    };
+
+    const deleteQuizAttempt = async (quizAttemptId) => {
+        await axios
+            .delete(
+                process.env.REACT_APP_BACKEND_API +
+                    `/quizAttempts/${quizAttemptId}`,
+                {
+                    headers: { Authorization: `Bearer ${currentToken}` },
+                    data: {
+                        userId: currUserId,
+                    },
+                }
+            )
+            .then(async (res) => {
+                if (res.data.success) {
+                    alert("Successfully deleted");
+                    await loadUserQuizAttempts(currUserId);
+                } else {
+                    alert("Failed to delete");
+                }
+            })
+            .catch((err) => {
+                alert("Failed to delete");
+            });
+    };
+
     return (
         <div>
-            <h1 className="title">All Quizzes</h1>
+            <h1 className="title">Quiz Management</h1>
+            <div className="tabs">
+                <ul>
+                    {tabs.map((tab, index) => {
+                        return (
+                            <li
+                                className={index == tab ? "is-active" : ""}
+                                key={index}
+                            >
+                                <a
+                                    onClick={() => {
+                                        setTab(index);
+                                    }}
+                                >
+                                    {tab}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="tabContents"></div>
             {loading ? (
                 <>Loading ...</>
             ) : (
                 <>
-                    <QuizList Contents={quizzes} DeleteContent={deleteQuiz} />
+                    {tab == 0 ? (
+                        <QuizList
+                            Contents={quizzes}
+                            DeleteContent={deleteQuiz}
+                        />
+                    ) : (
+                        <QuizAttemptList
+                            Contents={quizAttempts}
+                            DeleteContent={deleteQuizAttempt}
+                        />
+                    )}
                 </>
             )}
         </div>
