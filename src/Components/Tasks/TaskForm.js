37c150eb2be8665eb2e6b9bf7072374a84834cbf
Setsuna Yukiii <siang-meng@live.com>
2022-12-23 00:38:43 +0800

    refactored task form

--- src/Components/Tasks/TaskForm.js
+++ src/Components/Tasks/TaskForm.js
@@ -1,23 +1,38 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-function TaskForm({addTaskMethod}) {
+function TaskForm({ addTaskMethod }) {
     // props
-        // addTaskMethod --> adds task to DB
+    // addTaskMethod --> adds task to DB
 
     // state
-        // taskName --> name of task to add
+    // taskName --> name of task to add
 
     // methods
-        // add task --> adds new task by calling addTaskMethod (date is given date by default)--> emptys taskName upon successful adding
-    const [taskName,setTaskName] = useState("")
-    const addTask = async()=>{
-        await addTaskMethod(taskName)
-    }
+    // add task --> adds new task by calling addTaskMethod (date is given date by default)--> emptys taskName upon successful adding
+    const [taskName, setTaskName] = useState("");
+    const addTask = async () => {
+        await addTaskMethod(taskName).then(setTaskName(""));
+    };
     return (
         <div className="container taskFormContainer columns">
-            <input className="input column is-three-quarters" type="text" placeholder="Add new task name" onChange={(e)=>{setTaskName(e.target.value)}}/>
-            <button type="button" className="button button-link" onClick={()=>{addTask()}}>Add</button>
-            
+            <input
+                className="input column is-three-quarters"
+                type="text"
+                placeholder="Add new task name"
+                value={taskName}
+                onChange={(e) => {
+                    setTaskName(e.target.value);
+                }}
+            />
+            <button
+                type="button"
+                className="button addButton"
+                onClick={() => {
+                    addTask();
+                }}
+            >
+                Add
+            </button>
         </div>
     );
 }
